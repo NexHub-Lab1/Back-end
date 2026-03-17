bde05748b70b0b1bfd82acfb3c8b325522a71167
DHipo <bautidhipolito@gmail.com>
2026-03-17 11:53:21 -0300

    create user service and repo

---
+++ src/main/java/com/nexhub/backend/service/user/UserService.java
@@ -0,0 +1,34 @@
+package com.nexhub.backend.service.user;
+
+import com.nexhub.backend.model.User;
+import com.nexhub.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    }
+
+    public User addPoints(Long userId, Integer points) {
+        User user = getUserById(userId);
+        user.setTotal_points(user.getTotal_points() + points);
+        return userRepository.save(user);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
