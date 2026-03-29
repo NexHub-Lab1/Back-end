47f6a941914ed53e99ec7ad6993d1e2b39895dee
ManoloGioino <jmgioino@mail.austral.edu.ar>
2026-03-29 16:02:16 -0300

    Puse los campos de Project

--- src/main/java/com/nexhub/backend/model/Project.java
+++ src/main/java/com/nexhub/backend/model/Project.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import java.sql.Date;
+
 @Entity
 @Table(name = "projects")
 public class Project {
@@ -20,6 +22,10 @@ public class Project {
 
     @Getter
     @Setter
+    private Long OwnerId;
+
+    @Getter
+    @Setter
     private String name;
 
     @Getter
@@ -29,4 +35,25 @@ public class Project {
     @Getter
     @Setter
     private String githubRepo;
+
+    @Getter
+    @Setter
+    private String status;
+
+    @Getter
+    @Setter
+    private Date created_at;
+
+    @Getter
+    @Setter
+    private Date updated_at;
+
+    @Getter
+    @Setter
+    private Integer completed_task_count;
+
+    @Getter
+    @Setter
+    private Integer stars_count;
+
 }
